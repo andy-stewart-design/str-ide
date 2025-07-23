bb2a336dd18070cbcc00684e7eb8725999bb2672
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-07-23 14:56:17 -0400

    Adding audio controls

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -71,26 +71,9 @@ function App() {
     Object.values(editors()).forEach((ed) => ed.dispose());
   });
 
-  onRequestPlay(async () => {
-    const content = editors()[activeId()]?.getValue();
-    if (!content) return;
-    if (playingId() && activeId() !== playingId()) {
-      console.log("restarting");
-      strudel()?.stop();
-      await new Promise((r) => setTimeout(r, 100));
-    }
-    await strudel()?.evaluate(content);
-    if (strudel().scheduler.started) {
-      setPlaying(true);
-      setPlayingId(activeId());
-    }
-  });
+  onRequestPlay(handlePlay);
 
-  onRequestPause(() => {
-    strudel()?.stop();
-    setPlaying(false);
-    setPlayingId("");
-  });
+  onRequestPause(handlePause);
 
   function handleInitEditor(el: HTMLDivElement, tab: TabData) {
     const newEditor = initMonacoEditor(el);
@@ -159,6 +142,27 @@ function App() {
     }
   }
 
+  async function handlePlay() {
+    const content = editors()[activeId()]?.getValue();
+    if (!content) return;
+    if (playingId() && activeId() !== playingId()) {
+      console.log("restarting");
+      strudel()?.stop();
+      await new Promise((r) => setTimeout(r, 100));
+    }
+    await strudel()?.evaluate(content);
+    if (strudel().scheduler.started) {
+      setPlaying(true);
+      setPlayingId(activeId());
+    }
+  }
+
+  function handlePause() {
+    strudel()?.stop();
+    setPlaying(false);
+    setPlayingId("");
+  }
+
   return (
     <>
       <div id="navbar">
@@ -168,31 +172,61 @@ function App() {
       </div>
       <div id="tab-bar" data-visible={!!tabsArray().length}>
         <Show when={tabsArray().length}>
-          {tabsArray().map((tab) => (
-            <div class="tab">
-              <button
-                class="primary-action"
-                onClick={() => setActiveId(tab.id)}
-                data-active={activeId() === tab.id}
-              >
-                {tab.name ?? "new file"}
-              </button>
-              <button
-                class="secondary-action"
-                onClick={() => handleClose(tab.id)}
-                data-active={activeId() === tab.id}
-              >
-                <svg viewBox="0 0 16 16" width={16} height={16}>
-                  <path
-                    d="M 4 4 L 12 12 M 4 12 L 12 4"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width={1.5}
-                  />
-                </svg>
-              </button>
-            </div>
-          ))}
+          <div>
+            {tabsArray().map((tab) => (
+              <div class="tab">
+                <button
+                  class="primary-action"
+                  onClick={() => setActiveId(tab.id)}
+                  data-active={activeId() === tab.id}
+                >
+                  {tab.name ?? "new file"}
+                </button>
+                <button
+                  class="secondary-action"
+                  onClick={() => handleClose(tab.id)}
+                  data-active={activeId() === tab.id}
+                >
+                  <svg viewBox="0 0 16 16" width={16} height={16}>
+                    <path
+                      d="M 4 4 L 12 12 M 4 12 L 12 4"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width={1.5}
+                    />
+                  </svg>
+                </button>
+              </div>
+            ))}
+          </div>
+          <div>
+            <button
+              class="media"
+              aria-label={playing() ? "update" : "play"}
+              onClick={handlePlay}
+            >
+              <svg width="16" height="16" viewBox="0 0 20 20">
+                <path
+                  data-icon="play"
+                  d="M16.4806 9.13176C17.1524 9.51565 17.1524 10.4844 16.4806 10.8682L5.49614 17.1451C4.82948 17.526 4 17.0446 4 16.2768L4 3.72318C4 2.95536 4.82948 2.47399 5.49614 2.85494L16.4806 9.13176Z"
+                  fill="currentColor"
+                />
+                <path
+                  data-icon="update"
+                  d="M19 10C19 14.9706 14.9706 19 10 19C7.17087 19 4.64922 17.6934 3 15.6543V19H1V13C1 12.7348 1.10543 12.4805 1.29297 12.293C1.48051 12.1054 1.73478 12 2 12H8V14H4.25586C5.52176 15.8143 7.62285 17 10 17C13.866 17 17 13.866 17 10H19ZM19 7C19 7.26522 18.8946 7.5195 18.707 7.70703C18.5195 7.89457 18.2652 8 18 8H12V6H15.7441C14.4782 4.1857 12.3772 3 10 3C6.13401 3 3 6.13401 3 10H1C1 5.02944 5.02944 1 10 1C12.8289 1 15.3508 2.30596 17 4.34473V1H19V7Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
+            <button class="media" aria-label="pause" onClick={handlePause}>
+              <svg width="16" height="16" viewBox="0 0 20 20">
+                <path
+                  d="M7 2C7.55228 2 8 2.44772 8 3V17C8 17.5523 7.55228 18 7 18H5C4.44772 18 4 17.5523 4 17V3C4 2.44772 4.44772 2 5 2H7ZM15 2C15.5523 2 16 2.44772 16 3V17C16 17.5523 15.5523 18 15 18H13C12.4477 18 12 17.5523 12 17V3C12 2.44772 12.4477 2 13 2H15Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
+          </div>
         </Show>
       </div>
       <div id="app" data-editable={Boolean(tabsArray().length)}>
