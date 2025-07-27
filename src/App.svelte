16ba8b53fb6aa0b5353c6541a6e5c79603105cd6
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-07-27 12:20:15 -0400

    experimenting

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,47 @@
 <script lang="ts">
-  let count = $state(0)
+  import type { FileData } from "@/types/file-data";
+  import type { Editor } from "@/types/monaco";
+
+  interface TabData extends FileData {
+    id: string;
+  }
+  type TabGroup = Record<string, TabData>;
+  type EditorGroup = Record<string, Editor>;
+  type ShaderState = "unmounted" | "playing" | "paused";
+
+  const {
+    onRequestNewFile,
+    openFile,
+    onFileOpened,
+    saveFile,
+    onRequestSave,
+    onFileSaved,
+    onRequestClose,
+    onRequestPlay,
+    onRequestPause,
+    onRequestPlayVisuals,
+    onRequestPauseVisuals,
+    removeAllListeners,
+    warnBeforeClosing,
+  } = window.electronAPI;
+
+  let tabs = $state<TabGroup>({});
+  let activeId = $state("");
+
+  onFileOpened((data) => {
+    const id = crypto.randomUUID();
+    tabs = { ...tabs, [id]: { id, ...data } };
+    activeId = id;
+  });
 </script>
 
 <div id="navbar"></div>
-<div class="tabs">
-  <button onclick={() => count++}>Count: {count}</button>
-</div>
+<div class="tabs"></div>
 
 <style>
-    .tabs {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+  .tabs {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
